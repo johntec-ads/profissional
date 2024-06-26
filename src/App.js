8e2feeda8ea9eac2aefb8e0e1f5b235872003da4
johntec-ads <johntec.ads@gmail.com>
2024-06-26 06:04:11 -0300

    upState

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* import "react,useState,useEffect" */
+import React, { useState, useEffect } from 'react';
+
+/* Declarando component principal */
+function App() {
+
+  return(
+    <div>
+      <h1> Projeto Profissão Dev </h1>
+    </div>
+  )
+}
+
+export default App ;
