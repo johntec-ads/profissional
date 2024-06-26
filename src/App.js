e261c2b9c7d72f30df3d9d3c9db09e5abaf26c78
John Marcelo <johntec.ads@gmail.com>
2024-06-26 15:51:25 -0300

    upProject

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@
 import React, { useState, useEffect } from 'react';
 
 /* Declarando component principal */
-function App() {
-/* Declarando os estdos */
-const [tarefas, setTarefas ] = useState([]);/* Array and Function */
+function App () {
+  /* Declarando os estdos */
+  const [ tarefas, setTarefas ] = useState( [ '1', '2', '3' ] );/* Array and Function */
+  const [ input, setInput ] = useState( '' )
+
 
   /* Area de rendetização */
-  return(
+  return (
     <div>
-      <h1> Projeto Profissão Dev </h1>
+      <h1> Cadastro de clientes </h1>
+
+      {/* simular um array e varrer com o map */ }
+      <ul>
+        {/* Chaves para JS */ }
+        { tarefas.map( ( item, index ) => (
+          <li key={ index }  > { item } </li>
+        ) ) }
+      </ul>
+
+
+      <input type='input' value={ input }
+        onChange={ ( e ) => e.target.value }>
+      </input>
+
+      <button>Registro</button>
+
     </div>
   )
 }
 
-export default App ;
+export default App;
