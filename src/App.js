d733ba1a3012a3c52685aec6ce0ce02ec98bd9d4
johntec-ads <johntec.ads@gmail.com>
2024-06-27 06:19:42 -0300

    upClass

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import React, { useState, useEffect } from 'react';
 
 /* Declarando component principal */
 function App () {
-  /* Declarando os estdos */
+  /* Declarando os estados */
   const [ tarefas, setTarefas ] = useState( [ '1', '2', '3' ] );/* Array and Function */
   const [ input, setInput ] = useState( '' )
+
+  /* Aqui atualizo o estado */
+  function registro() {
+    setTarefas({...tarefas, input});/* Operador spread para juntar os valores
+    do array e input para adicionar o novo valor */
+    setInput('')
+
+
+
+  }
 
 
   /* Area de rendetização */
@@ -13,20 +23,19 @@ function App () {
     <div>
       <h1> Cadastro de clientes </h1>
 
-      {/* simular um array e varrer com o map */ }
+      {/* simular um array e varrendo com o map */ }
       <ul>
         {/* Chaves para JS */ }
+        {/* Param para os itens e index para a key */}
         { tarefas.map( ( item, index ) => (
           <li key={ index }  > { item } </li>
         ) ) }
       </ul>
 
+      <input  type='text' value={input} 
+      onChange={ (e) => setInput(e.target.value)} ></input>      
 
-      <input type='input' value={ input }
-        onChange={ ( e ) => e.target.value }>
-      </input>
-
-      <button>Registro</button>
+      <button type='button' onClick={registro}  >Registro</button>
 
     </div>
   )
