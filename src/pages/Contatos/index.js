1617a1d8d5048ee4664d87b7552108c0ebacffdb
johntec-ads <johntec.ads@gmail.com>
2024-07-10 06:53:05 -0300

    upClass

---
+++ src/pages/Contatos/index.js
@@ -0,0 +1,14 @@
+
+function Contatos () {
+  return(
+    <div>
+      <h1>
+        Hello World
+      </h1>
+    </div>
+  
+  )
+
+}
+
+export default Contatos ;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+
+function Home () {
+  return(
+    <div>
+      <h1>
+        Hello World
+      </h1>
+    </div>
+  
+  )
+
+}
+
+export default Home ;
